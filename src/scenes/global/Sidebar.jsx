ead27b4b615f59dd233e1ea89d98de7f348bf49b
Amar Singh Yadav <amarprince9@gmail.com>
2023-03-09 21:13:14 +0530

    Fixed Sidebar menu active

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
-import { Box, colors, IconButton, Typography, useTheme } from '@mui/material';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { tokens } from '../../theme';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
@@ -16,11 +16,10 @@ import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
-import { color } from '@mui/system';
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
-  const color = tokens(theme.palette.mode);
+  const colors = tokens(theme.palette.mode);
   return (
     <MenuItem
       active={selected === title}
@@ -39,7 +38,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
 const Sidebar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [isCollapsed, setIsCollapsed] = useState();
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const [selected, setSelected] = useState('Dashboard');
 
   return (
@@ -56,7 +55,7 @@ const Sidebar = () => {
           color: '#868dfb !important',
         },
         '& .pro-menu-item.active': {
-          colors: '#6870fa !important',
+          color: '#6870fa !important',
         },
       }}
     >
