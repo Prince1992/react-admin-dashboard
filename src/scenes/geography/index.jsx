99fad19b699aaf88a8e47e2326f4d2bd4810644d
Amar Singh Yadav <amarprince9@gmail.com>
2023-03-09 16:45:29 +0530

    Dashboard component

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -8,12 +8,12 @@ const Geography = () => {
   const colors = tokens(theme.palette.mode);
   return (
     <Box m="20px">
-      <Header title="Geography" subtitle="Simple Geography Chart" />
+      <Header title="Geography" subTitle="Simple Geography Chart" />
 
       <Box
         height="75vh"
         border={`1px solid ${colors.grey[100]}`}
-        borderRadius="4px"
+        borderRadius="5px"
       >
         <GeographyChart />
       </Box>
